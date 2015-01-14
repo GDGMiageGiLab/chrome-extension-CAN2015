e043ff05e5a99792d9321cf6b07b1af1a254c894
ericzile <ericzile90@gmail.com>
2015-01-14 01:59:20 +0000

    Ajout des element de Colombe
    Ajout de l'historique, Equipe et autre

--- js/sidebarEffects.js
+++ js/sidebarEffects.js
@@ -25,7 +25,7 @@
 		return check;
 	}
 
-	function init() {
+	function init_button() {
 
 		var container = document.getElementById( 'st-container' ),
 			buttons = Array.prototype.slice.call( document.querySelectorAll( '#st-trigger-effects > button' ) ),
@@ -58,6 +58,41 @@
 
 	}
 
-	init();
+	function init_label() {
+
+		var container = document.getElementById( 'st-container' ),
+			labels = Array.prototype.slice.call( document.querySelectorAll( 'label' ) ),
+			// event type (if mobile use touch events)
+			eventtype = mobilecheck() ? 'touchstart' : 'click',
+			resetMenu = function() {
+				classie.remove( container, 'st-menu-open' );
+			},
+			bodyClickFn = function(evt) {
+				if( !hasParentClass( evt.target, 'st-menu' ) ) {
+					resetMenu();
+					document.removeEventListener( eventtype, bodyClickFn );
+				}
+			};
+
+		labels.forEach( function( el, i ) {
+			var effect = el.getAttribute( 'data-effect' );
+
+			el.addEventListener( eventtype, function( ev ) {
+				ev.stopPropagation();
+				ev.preventDefault();
+				container.className = 'st-container'; // clear
+				classie.add( container, effect );
+				setTimeout( function() {
+					classie.add( container, 'st-menu-open' );
+				}, 25 );
+				document.addEventListener( eventtype, bodyClickFn );
+			});
+		} );
+
+	}
+
+	
+	init_button();
+	init_label();
 
 })();
